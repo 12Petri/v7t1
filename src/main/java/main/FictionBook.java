fee09c67b32af7b0fd5f3da03251e4100aaa39d8
12Petri <petri.majamaa@student.lut.fi>
2025-03-03 16:50:09 +0200

    Versio3

--- src/main/java/main/FictionBook.java
+++ src/main/java/main/FictionBook.java
@@ -19,7 +19,7 @@ public class FictionBook extends Book implements Borrowable {
             System.out.println("Kirja " + title + " on nyt lainattu.");
         }
         else {
-            System.out.println("Kirjaa " + title + " ei enää ole lainattavissa!");
+            System.out.println("Kirjaa " + title + " ei enään ole lainattavissa!");
         }
     }
 
